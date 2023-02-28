dde39e985f8d8367ba796dbed70018577d51b9ec
YeahDreamItPossible <yeahdreamitpossible@gmail.com>
2023-02-28 20:28:07 +0800

    新增pinia目录

--- v3/pinia-2/packages/nuxt/src/runtime/composables.ts
+++
@@ -1,4 +0,0 @@
-import { useNuxtApp } from '#app'
-export * from 'pinia'
-
-export const usePinia = () => useNuxtApp().$pinia
--- v3/pinia-2/packages/pinia/__tests__/vitest-setup.ts
+++
@@ -1,6 +0,0 @@
-import { beforeEach } from 'vitest'
-import { setActivePinia } from '../src'
-
-beforeEach(() => {
-  setActivePinia(undefined)
-})
