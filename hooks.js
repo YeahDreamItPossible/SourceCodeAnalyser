f9eae752a57a8d0ea1e0251db8873341bb53d910
newStar_Lee <69293051@qq.com>
2023-11-27 16:28:37 +0800

    代码格式化

--- hooks.js
+++ hooks.js
@@ -2,7 +2,7 @@ const hooks = `
 environment                   // 空调用
 afterEnvironment              // 空调用
 entryOption
-afterPlugins                  // 模块联邦 暂时不详 需要好好研究研究
+afterPlugins                  // 初始化内部插件后调用
 afterResolvers                // 空调用
 initialize                    // 空调用
 beforeRun                     // 标识 compiler 开始 主要是调用 NodeEnvironmentPlugin 插件
@@ -14,113 +14,113 @@ compile                       // 主要时 从输出的bundle排除依赖(该依
 thisCompilation               // 主要是给 compilation hooks 不同 hook 注册函数
 compilation                   // 主要是给 compilation hooks 不同 hook 注册函数
 make                          // 添加入口 开始编译 主要是调用 compilation.addEntry
-      addEntry                // 空调用 标记
-      buildModule
-      normalModuleLoader
-      succeedModule
-      ...(循环 buildModule normalModuleLoader succeedModule)
+		addEntry                // 空调用 标记
+		buildModule
+		normalModuleLoader
+		succeedModule
+		...(循环 buildModule normalModuleLoader succeedModule)
 finishMake
-      log
-      finishModules
-      log
-      seal                    // 主要是收集errors 和 warnings
-      optimizeDependencies    // 仍然是收集errors 和 warnings
-      log
-      afterOptimizeDependencies   // 空调用
-      log
-      beforeChunks            // 空调用
-      log
-      afterChunks             // 空调用 (在beforeChunks 和 afterChunks 完成 chunks)
-      log
-      optimize                // 空调用
-      optimizeModules         // 空调用
-      afterOptimizeModules    // 空调用
-      optimizeChunks          // 优化chunks
-                              // 串行调用插件 
-                              // EnsureChunkConditionsPlugin 
-                              // RemoveEmptyChunksPlugin 
-                              // MergeDuplicateChunksPlugin
-                              // SplitChunksPlugin
-                              // RemoveEmptyChunksPlugin
-      log
-      afterOptimizeChunks     // 空调用
-      optimizeTree            // 直接执行回调
-      afterOptimizeTree       // 空调用
-      optimizeChunkModules    // 直接执行回调
-      afterOptimizeChunkModules   // 空调用
-      shouldRecord            // 空调用
-      reviveModules           // 空调用
-      beforeModuleIds         // 空调用
-      moduleIds               // 调用来每个模块分配一个 id(NamedModuleIdsPlugin)
-      optimizeModuleIds       // 空调用
-      afterOptimizeModuleIds  // 空调用
-      reviveChunks
-      beforeChunkIds
-      chunkIds
-      optimizeChunkIds
-      afterOptimizeChunkIds
-      recordModules
-      recordChunks
-      optimizeCodeGeneration
-      log
-      beforeModuleHash
-      log
-      afterModuleHash
-      log
-      beforeCodeGeneration
-      log
-      afterCodeGeneration
-      log
-      beforeRuntimeRequirements
-      additionalModuleRuntimeRequirements
-      ...
-      additionalTreeRuntimeRequirements
-      runtimeModule
-      afterRuntimeRequirements
-      log
-      beforeHash
-      log
-      log
-      chunkHash
-      contentHash
-      log
-      log
-      fullHash
-      log
-      log
-      afterHash
-      log
-      log
-      recordHash
-      log
-      beforeModuleAssets
-      log
-      shouldGenerateChunkAssets
-      beforeChunkAssets
-      renderManifest
-      assetPath
-      chunkAsset
-      log
-      optimizeAssets
-      processAssets
-      afterOptimizeAssets
-      afterProcessAssets
-      log
-      record
-      needAdditionalSeal
-      afterSeal
-      log
-      ...
-      log
+		log
+		finishModules
+		log
+		seal                    // 主要是收集errors 和 warnings
+		optimizeDependencies    // 仍然是收集errors 和 warnings
+		log
+		afterOptimizeDependencies   // 空调用
+		log
+		beforeChunks            // 空调用
+		log
+		afterChunks             // 空调用 (在beforeChunks 和 afterChunks 完成 chunks)
+		log
+		optimize                // 空调用
+		optimizeModules         // 空调用
+		afterOptimizeModules    // 空调用
+		optimizeChunks          // 优化chunks
+														// 串行调用插件 
+														// EnsureChunkConditionsPlugin 
+														// RemoveEmptyChunksPlugin 
+														// MergeDuplicateChunksPlugin
+														// SplitChunksPlugin
+														// RemoveEmptyChunksPlugin
+		log
+		afterOptimizeChunks     // 空调用
+		optimizeTree            // 直接执行回调
+		afterOptimizeTree       // 空调用
+		optimizeChunkModules    // 直接执行回调
+		afterOptimizeChunkModules   // 空调用
+		shouldRecord            // 空调用
+		reviveModules           // 空调用
+		beforeModuleIds         // 空调用
+		moduleIds               // 调用来每个模块分配一个 id(NamedModuleIdsPlugin)
+		optimizeModuleIds       // 空调用
+		afterOptimizeModuleIds  // 空调用
+		reviveChunks
+		beforeChunkIds
+		chunkIds
+		optimizeChunkIds
+		afterOptimizeChunkIds
+		recordModules
+		recordChunks
+		optimizeCodeGeneration
+		log
+		beforeModuleHash
+		log
+		afterModuleHash
+		log
+		beforeCodeGeneration
+		log
+		afterCodeGeneration
+		log
+		beforeRuntimeRequirements
+		additionalModuleRuntimeRequirements
+		...
+		additionalTreeRuntimeRequirements
+		runtimeModule
+		afterRuntimeRequirements
+		log
+		beforeHash
+		log
+		log
+		chunkHash
+		contentHash
+		log
+		log
+		fullHash
+		log
+		log
+		afterHash
+		log
+		log
+		recordHash
+		log
+		beforeModuleAssets
+		log
+		shouldGenerateChunkAssets
+		beforeChunkAssets
+		renderManifest
+		assetPath
+		chunkAsset
+		log
+		optimizeAssets
+		processAssets
+		afterOptimizeAssets
+		afterProcessAssets
+		log
+		record
+		needAdditionalSeal
+		afterSeal
+		log
+		...
+		log
 afterCompile    // 直接执行回调
-      log
+		log
 shouldEmit      // 空调用
-emit            // 直接执行回调 此回调函数中 创建目标文件 并输出结果
-      assetPath
+emit            // 直接执行回调 此回调函数中 创建目标目录 并输出结果
+		assetPath
 assetEmitted    // 输出每个文件后
 afterEmit       // 直接执行回调
-      log
-      needAdditionalPass
+		log
+		needAdditionalPass
 `;
 
 const compilation = `
@@ -165,5 +165,4 @@ const compilation = `
 
 // Dependency 位置信息
 
-Module => DependenciesBlock => Dependency
-       => AsyncDependenciesBlock =>  DependenciesBlock => Dependency
+Module => DependenciesBlock => Dependency => AsyncDependenciesBlock => DependenciesBlock => Dependency
